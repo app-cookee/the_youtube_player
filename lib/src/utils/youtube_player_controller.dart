8d4e1028dba7c0b16947bfdfa64f15d41f56052d
adarsh <adarsh@cookee.io>
2024-09-24 09:40:57 +0530

    InAppWebView updated

--- lib/src/utils/youtube_player_controller.dart
+++ lib/src/utils/youtube_player_controller.dart
@@ -4,7 +4,7 @@
 
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:zikzak_inappwebview/zikzak_inappwebview.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 import '../enums/playback_rate.dart';
 import '../enums/player_state.dart';
